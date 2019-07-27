9030a6b003ef0003e4b2a4527f4fa5f5e3291d1d
vipcxj <vipcxj@126.com>
2019-07-28 00:38:00 +0800

    feature: flex mode feature: auto font size

--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,115 @@
 import React, {forwardRef, Ref, useImperativeHandle, useLayoutEffect, useRef, useState} from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 
+const PIXELS_PER_INCH = 96;
+const EL_FOR_TEST_DEFAULT_FONTSIZE = 'el-for-test-default-font-size';
+function getDefaultFontSize(){
+    let test = document.getElementById(EL_FOR_TEST_DEFAULT_FONTSIZE);
+    if (!test) {
+        test = document.createElement('div');
+        test.id = EL_FOR_TEST_DEFAULT_FONTSIZE;
+        test.style.cssText = 'display:inline-block; padding:0; line-height:1; position:absolute; visibility:hidden; font-size:1em';
+        test.appendChild(document.createTextNode('M'));
+        document.body.appendChild(test);
+    }
+    return Number.parseFloat(getComputedStyle(test).fontSize);
+}
+
+function toPx(value: string | number): number {
+    if (typeof value === 'number') {
+        return value;
+    }
+    if (value.endsWith('px')) {
+        return Number.parseFloat(value);
+    } else if (value.endsWith('pt')) {
+        return Number.parseFloat(value) * PIXELS_PER_INCH / 72;
+    } else if (value.endsWith('in')) {
+        return Number.parseFloat(value) * PIXELS_PER_INCH;
+    } else if (value.endsWith('cm')) {
+        return Number.parseFloat(value) * PIXELS_PER_INCH / 2.54;
+    } else if (value.endsWith('mm')) {
+        return Number.parseFloat(value) * PIXELS_PER_INCH / 25.4;
+    } else if (value.endsWith('pc')) {
+        return Number.parseFloat(value) * PIXELS_PER_INCH / 6;
+    } else if (value.endsWith('em') || value.endsWith('rem') || value.endsWith('%')) {
+        const em = getDefaultFontSize();
+        if (value.endsWith('em') || value.endsWith('rem')) {
+            return Number.parseFloat(value) * em;
+        } else {
+            return Number.parseFloat(value) * em / 100;
+        }
+    } else {
+        return Number.NaN;
+    }
+}
+
+const setSpanText = (span: HTMLSpanElement, text: string) => {
+    span.innerText = text;
+};
+
+const adjustFontSize = (vcSpan: HTMLSpanElement, baseFontSize: number, minFontSize: number, cWidth: number) => {
+    let vcWidth = vcSpan.getClientRects()[0].width;
+    if (vcWidth !== cWidth) {
+        let fontSize = Number.parseFloat(getComputedStyle(vcSpan).fontSize);
+        if (vcWidth > cWidth) {
+            while (vcWidth > cWidth && fontSize > minFontSize) {
+                fontSize = Math.max(fontSize * 0.95, minFontSize);
+                vcSpan.style.fontSize = `${fontSize}px`;
+                vcWidth = vcSpan.getClientRects()[0].width;
+            }
+        } else {
+            let lstFontSize;
+            while (vcWidth < cWidth && fontSize < baseFontSize) {
+                lstFontSize = fontSize;
+                fontSize = Math.min(fontSize * 1.05, baseFontSize);
+                vcSpan.style.fontSize = `${fontSize}px`;
+                vcWidth = vcSpan.getClientRects()[0].width;
+            }
+            if (vcWidth > cWidth) {
+                fontSize = lstFontSize;
+                vcSpan.style.fontSize = `${fontSize}px`;
+            }
+        }
+        return fontSize;
+    } else {
+        return undefined;
+    }
+};
+
 const calcShowText = (span: HTMLSpanElement, text: string, cWidth: number, eWidth: number) => {
-    const lstShowText: string | null = span.textContent;
+    const lstShowText: string | null = span.innerText;
     const cWidth2 = Math.max(cWidth - eWidth, 0);
     let end: number = (lstShowText && lstShowText.length) || text.length;
     let showText: string = lstShowText || text;
-    span.textContent = showText;
+    setSpanText(span, showText);
     let sWidth: number = span.getClientRects()[0].width;
-    if (end > 0 && sWidth > cWidth) {
+    if (sWidth > cWidth) {
         end = Math.floor(cWidth2 / sWidth * end);
         showText = text.substring(0, end);
-        span.textContent = showText;
+        setSpanText(span, showText);
         sWidth = span.getClientRects()[0].width;
-        if (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
-            while (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
-                --end;
-                showText = text.substring(0, end);
-                span.textContent = showText;
-                sWidth = span.getClientRects()[0].width;
-            }
-        } else if (end < text.length && sWidth < cWidth2) {
-            while (end < text.length && sWidth < cWidth2) {
-                ++end;
-                showText = text.substring(0, end);
-                span.textContent = showText;
-                sWidth = span.getClientRects()[0].width;
-            }
-            if (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
-                --end;
-                showText = text.substring(0, end);
-                span.textContent = showText;
-            }
-        }
-    } else if (end < text.length && sWidth < cWidth) {
+    } else if (sWidth < cWidth) {
         end = Math.max(Math.min(Math.ceil(cWidth2 / sWidth * end), text.length), end + 1);
         showText = text.substring(0, end);
-        span.textContent = showText;
+        setSpanText(span, showText);
         sWidth = span.getClientRects()[0].width;
-        if (end < text.length && sWidth < cWidth2) {
-            while (end < text.length && sWidth < cWidth2) {
-                ++end;
-                showText = text.substring(0, end);
-                span.textContent = showText;
-                sWidth = span.getClientRects()[0].width;
-            }
-            if (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
-                --end;
-                showText = text.substring(0, end);
-                span.textContent = showText;
-            }
-        } else if (end > 0) {
-            while (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
-                --end;
-                showText = text.substring(0, end);
-                span.textContent = showText;
-                sWidth = span.getClientRects()[0].width;
-            }
-        }
+    }
+    while (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
+        --end;
+        showText = text.substring(0, end);
+        setSpanText(span, showText);
+        sWidth = span.getClientRects()[0].width;
+    }
+    while (end < text.length && sWidth < cWidth2) {
+        ++end;
+        showText = text.substring(0, end);
+        setSpanText(span, showText);
+        sWidth = span.getClientRects()[0].width;
+    }
+    if (end > 0 && ((end === text.length && sWidth > cWidth) || (end !== text.length && sWidth > cWidth2))) {
+        --end;
+        showText = text.substring(0, end);
+        setSpanText(span, showText);
     }
     return showText;
 };
@@ -70,10 +122,13 @@ export interface IEllipsis {
 
 export interface IEllipsisProps {
     text?: string;
+    minFontSize?: number | string;
+    minFontSizeRadio?: number;
     style?: React.CSSProperties;
     className?: string;
     ellipsis?: React.ReactNode;
     onEllipsis?: (showEllipsis: boolean, showText: string, text?: string) => void;
+    flex?: boolean;
 }
 
 const virtualBlockStyle: React.CSSProperties = {
@@ -81,21 +136,28 @@ const virtualBlockStyle: React.CSSProperties = {
     overflow: 'hidden',
     position: 'fixed',
     top: '-9999px',
-    whiteSpace: 'nowrap',
+    whiteSpace: 'pre',
 };
 
-const defaultEllipsis = <span> ...</span>;
+const showTextStyle: React.CSSProperties = {
+    whiteSpace: 'pre',
+};
+
+const defaultEllipsis = <span>...</span>;
 
 const checkShowEllipsis = (showText?: string, text?: string) => {
     return (showText || '') !== (text || '');
 };
 
-export default forwardRef(({text, style, onEllipsis, ellipsis = defaultEllipsis, ...rest}: IEllipsisProps, ref: Ref<IEllipsis>) => {
+export default forwardRef(({text, minFontSize, minFontSizeRadio, style, onEllipsis, ellipsis = defaultEllipsis, flex = false, ...rest}: IEllipsisProps, ref: Ref<IEllipsis>) => {
     const cRef: Ref<HTMLSpanElement> = useRef<HTMLSpanElement>(null);
+    const vcRef: Ref<HTMLSpanElement> = useRef<HTMLSpanElement>(null);
     const vRef: Ref<HTMLSpanElement> = useRef<HTMLSpanElement>(null);
     const eRef: Ref<HTMLSpanElement> = useRef<HTMLSpanElement>(null);
-    const [showText, setShowText] = useState();
-    const [refresh, setRefresh] = useState(true);
+    const sRef: Ref<HTMLSpanElement> = useRef<HTMLSpanElement>(null);
+    const eRef1: Ref<HTMLSpanElement> = useRef<HTMLSpanElement>(null);
+    const [showText, setShowText] = useState<string>();
+    const [refresh, setRefresh] = useState<boolean>(true);
     const showEllipsis = checkShowEllipsis(showText, text);
     useImperativeHandle(ref, () => ({
         get showText() { return showText || ''; },
@@ -103,14 +165,29 @@ export default forwardRef(({text, style, onEllipsis, ellipsis = defaultEllipsis,
         update: () => setRefresh(!refresh),
     }), [text, showText, refresh]);
     useLayoutEffect(() => {
-        const cWidth = cRef.current!.getClientRects()[0].width;
+        const cSpan = cRef.current!;
+        const vcSpan = vcRef.current!;
+        const cWidth = cSpan.getClientRects()[0].width;
         const span = vRef.current!;
         const eWidth = eRef.current!.getClientRects()[0].width;
         const observer = new ResizeObserver(() => {
-            const rect = cRef.current!.getClientRects()[0];
+            const rect = cSpan.getClientRects()[0];
             if (rect) {
                 const cw = rect.width;
-                const st1 = calcShowText(span, text || '', cw, eWidth);
+                if (minFontSizeRadio || minFontSize) {
+                    const baseFontSize = Number.parseFloat(getComputedStyle(cSpan).fontSize);
+                    const minFontSizePx = Math.max(minFontSize && toPx(minFontSize) || baseFontSize * Math.max(Math.min(minFontSizeRadio || 1, 1), 0), 1);
+                    const fs = adjustFontSize(vcSpan, baseFontSize, minFontSizePx, cw);
+                    if (fs !== undefined) {
+                        sRef.current!.style.fontSize = `${fs}px`;
+                        eRef1.current!.style.fontSize = `${fs}px`;
+                    }
+                } else {
+                    vcSpan.style.fontSize = undefined;
+                    sRef.current!.style.fontSize = undefined;
+                    eRef1.current!.style.fontSize = undefined;
+                }
+                const st1 = calcShowText(span, text || '', cw, eRef.current!.getClientRects()[0].width);
                 const se1 = checkShowEllipsis(st1, text);
                 if (onEllipsis) {
                     onEllipsis(se1, st1, text);
@@ -128,16 +205,21 @@ export default forwardRef(({text, style, onEllipsis, ellipsis = defaultEllipsis,
         return () => {
             observer.disconnect();
         }
-    }, [text, refresh, ellipsis]);
+    }, [text, refresh, ellipsis, minFontSizeRadio, minFontSize]);
+    const containerStyle = style ? {
+        display: flex ? 'flex' : 'inline-block',
+        ...style,
+    } : {
+        display: flex ? 'flex' : 'inline-block',
+    };
     return (
-        <span ref={cRef} style={style ? {display: 'inline-block', ...style} : {display: 'inline-block'}} {...rest}>
-            <span key="virtualShowText" ref={vRef} style={virtualBlockStyle} />
-            <span key="virtualEllipsis" style={virtualBlockStyle}>
-                test
+        <span ref={cRef} style={containerStyle} {...rest}>
+            <span key="virtualEllipsis" ref={vcRef} style={virtualBlockStyle}>
+                <span ref={vRef} />
                 <span ref={eRef}>{ ellipsis }</span>
             </span>
-            <span key="showText">{showText}</span>
-            <span key="ellipsis">
+            <span key="showText" style={showTextStyle} ref={sRef}>{showText}</span>
+            <span key="ellipsis" ref={eRef1}>
                 {showEllipsis ? ellipsis : null}
             </span>
         </span>
--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { number, text, radios } from '@storybook/addon-knobs';
+import { number, text, radios, boolean } from '@storybook/addon-knobs';
 import Ellipsis from '../src';
 
 const createWidthType = (label, group) => {
@@ -12,9 +12,28 @@ const createWidth = (label, group, widthType, value) => {
     case 'pixel':
       return number(label, value, {}, group);
     case 'percent':
-      return `${number(label, value, { range: true, min: 0, max: 100, step: 1 }, group)}%`;
+      return `${number(label, 100, { range: true, min: 0, max: 100, step: 1 }, group)}%`;
     default:
       return 300;
+  }
+};
+
+const createMinFontSizeType = (label, group) => {
+  return radios(label, { Size: 'size', Radio: 'radio', None: 'none' }, 'none', group);
+};
+
+const createMinFontSize = (label, group, type) => {
+  switch (type) {
+    case 'size':
+      return {
+        minFontSize: text(label, undefined, group),
+      };
+    case 'radio':
+      return {
+        minFontSizeRadio: number(label, 1, { range: true, min: 0, max: 1, step: 0.1 }, group),
+      };
+    default:
+      return  {};
   }
 };
 
@@ -24,10 +43,13 @@ storiesOf('ellipsis', module)
     () => {
       const widthType = createWidthType('Width Type', 'Width');
       const minWidthType = createWidthType('Min Width Type', 'Min Width');
+      const minFontSizeType = createMinFontSizeType('Min Font Size Type', 'Text');
       return (
         <Ellipsis
           style={{ width: createWidth('Width', 'Width', widthType, 300), minWidth: createWidth('Min Width', 'Min Width', minWidthType, 50) }}
           text={text('Text', 'gdlgjj;l 尽量少干 根 kjlsjg g 00 -s9 0 -- ds-g-sdg-sg-', 'Text')}
+          flex={boolean('flex', false, 'Base')}
+          { ...createMinFontSize('Min Font Size', 'Text', minFontSizeType) }
         />
       );
     },
@@ -37,11 +59,14 @@ storiesOf('ellipsis', module)
     () => {
       const widthType = createWidthType('Width Type', 'Width');
       const minWidthType = createWidthType('Min Width Type', 'Min Width');
+      const minFontSizeType = createMinFontSizeType('Min Font Size Type', 'Text');
       return (
         <Ellipsis
           style={{ width: createWidth('Width', 'Width', widthType, 300), minWidth: createWidth('Min Width', 'Min Width', minWidthType, 50) }}
           text={text('Text', 'gdlgjj;l 尽量少干 根 kjlsjg g 00 -s9 0 -- ds-g-sdg-sg-', 'Text')}
           ellipsis=" More"
+          flex={boolean('flex', false, 'Base')}
+          { ...createMinFontSize('Min Font Size', 'Text', minFontSizeType) }
         />
       );
     },
